20bad7ed79c4a2bf0ba66de2e3f422c3fca646fb
Anton Augustsson <anton.augustsson99@gmail.com>
2024-10-26 18:06:48 +0200

    initial implementation of state class and beter gsm print

--- example.cc
+++ example.cc
@@ -5,6 +5,56 @@
 #include <functional>
 #include <iostream>
 
+void s1_enter() {
+    printf("enter state 1");
+}
+
+void s1_execute() {
+    printf("executing state 1");
+}
+
+void s1_exit() {
+    printf("exit state 1");
+}
+
+void s2_enter() {
+    printf("enter state 2");
+}
+
+void s2_execute() {
+    printf("executing state 2");
+}
+
+void s2_exit() {
+    printf("exit state 2");
+}
+
+void s3_enter() {
+    printf("enter state 3");
+}
+
+void s3_execute() {
+    printf("executing state 3");
+}
+
+void s3_exit() {
+    printf("exit state 3");
+}
+
+void s4_enter() {
+    printf("enter state 4");
+}
+
+void s4_execute() {
+    printf("executing state 4");
+}
+
+void s4_exit() {
+    printf("exit state 4");
+}
+
+
+
 
 int main() {
     int x; // This will change and influence the state matrix
@@ -12,7 +62,12 @@ int main() {
 
     const std::size_t num_states = 4U;
 
-    State s1, s2, s3, s4;
+
+    State s1(s1_enter, s1_execute, s1_exit);
+    State s2(s2_enter, s2_execute, s2_exit);
+    State s3(s3_enter, s3_execute, s3_exit);
+    State s4(s4_enter, s4_execute, s4_exit);
+
     States_t<num_states> states = {s1, s2, s3, s4};
 
     // 2D array of BoolExpr
@@ -25,12 +80,14 @@ int main() {
 
     GSM state_machine(state_transition_table, states);
     
+    state_machine.print_state_transition_stable();
+
     // Change x and print the matrix
     x = 3;
-    state_machine.print();
+    state_machine.print_evaluates();
    
     x = 4;
-    state_machine.print();
+    state_machine.print_evaluates();
 
     return 0;
 }
--- gsm.hpp
+++ gsm.hpp
@@ -4,16 +4,27 @@
 #include "expr.hpp"
 #include <functional>
 
+using func_t = std::function<void()>;
 
 class State {
   public:
-    int id;
+    State(func_t enter_func_param, func_t execute_func_param, func_t exit_func_param) 
+      : enter_func(enter_func_param), execute_func(enter_func_param), exit_func(enter_func_param) {}
 
-    void enter();
-    void execute();
-    void exit();
+    void enter() const { enter_func(); };
+    void execute() const { execute_func(); };
+    void exit() const { exit_func(); };
+
+  private:
+    int id;
+    int period_ms;
+
+    func_t enter_func;
+    func_t execute_func;
+    func_t exit_func;
 };
 
+/* TODO: add later
 class StatePeriodic: public State {
   public:
     int period_ms;
@@ -23,6 +34,7 @@ class StateContinues: public State {
   public:
     // Not sure but should be that a periodic state always finnish but continues we need to stop it. Is that not exit
 };
+*/
 
 template <std::size_t NUM_STATES>
 using StateTable_t = std::array<std::array<BoolExpr, NUM_STATES>, NUM_STATES>;
@@ -38,12 +50,38 @@ class GSM {
       : state_transition_table(stt), states(s) {}
 
     // TODO: this evaluates so... should be mentioned in the name?
-    void print() {
+    void print_state_transition_stable() {
+      std::cout << "\t";
       for (std::size_t i = 0; i < NUM_STATES; ++i) {
+        std::cout << "s" << i+1 << "\t\t";
+      }
+      std::cout << std::endl;
+
+      for (std::size_t i = 0; i < NUM_STATES; ++i) {
+        std::cout << "s" << i+1 << "\t";
+
         for (std::size_t j = 0; j < NUM_STATES; ++j) {
-          std::cout << "state_table[" << i << "][" << j << "] = ";
-          state_transition_table[i][j].print();  // Print the expression
-          std::cout << " (evaluates to " << state_transition_table[i][j].evaluate() << ")  ";
+          state_transition_table[i][j].print();
+          std::cout << "\t\t";
+        }
+        std::cout << std::endl;
+      }
+      std::cout << std::endl;
+    }
+
+    void print_evaluates() {
+      std::cout << "\t";
+      for (std::size_t i = 0; i < NUM_STATES; ++i) {
+        std::cout << "s" << i+1 << "\t\t";
+      }
+      std::cout << std::endl;
+
+      for (std::size_t i = 0; i < NUM_STATES; ++i) {
+        std::cout << "s" << i+1 << "\t";
+
+        for (std::size_t j = 0; j < NUM_STATES; ++j) {
+          std::cout << state_transition_table[i][j].evaluate();
+          std::cout << "\t\t";
         }
         std::cout << std::endl;
       }
